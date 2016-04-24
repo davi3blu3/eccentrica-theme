8f15a1afffa422ae64928312fca329795a23b653
Dave Harned <dharned@gmail.com>
2016-04-24 13:47:09 -0500

    create custom body class

--- header.php
+++ header.php
@@ -6,6 +6,18 @@
 
     <?php wp_head(); ?>
 </head>
-<body>
+
+<?php
+	
+	if ( is_front_page() ):
+		$eccentrica_body_classes = array('eccentrica-home-pg');
+	elseif ( is_home() ): 
+		$eccentrica_body_classes = array('eccentrica-blog-pg');
+	else:
+		$eccentrica_body_classes = array('eccentrica-secondary-pg');
+	endif;
+?>
+
+<body <?php body_class( $eccentrica_body_classes ); ?>>
 
 	<?php wp_nav_menu(array('theme_location'=>'primary')); ?>
