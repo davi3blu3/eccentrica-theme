f6bb360159a8b73bc518749344f10cb880889df6
Dave Harned <dharned@gmail.com>
2016-04-24 13:36:39 -0500

    add post loop, assign to blog page

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <?php get_header(); ?>
 
-<h1>This is my index</h1>
+<?php
+
+	if ( have_posts() ):
+
+		while ( have_posts() ): the_post(); ?>
+
+			<h3><?php the_title(); ?></h3>
+			<small>Posted on <?php the_time('F j, Y'); ?></small>
+
+			<p><?php the_content(); ?></p>
+
+		<?php endwhile;
+
+	endif;
+
+?>
 
 <?php get_footer(); ?>
