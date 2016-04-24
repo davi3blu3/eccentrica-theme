45c87d6f955e58bdb2a3a85a61972a2edf346de3
Dave Harned <dharned@gmail.com>
2016-04-24 13:06:29 -0500

    add custom menus

--- footer.php
+++ footer.php
@@ -1,5 +1,6 @@
 	<footer>
 		<h4>&copy; 2016 Dave Harned</h4>
+		<?php wp_nav_menu(array('theme_location'=>'secondary')); ?>
 	</footer>
 
 	<?php wp_footer(); ?>
--- functions.php
+++ functions.php
@@ -4,9 +4,18 @@ function eccentrica_script_enqueue() {
 
 	wp_enqueue_style('customstyle', get_template_directory_uri() . '/css/eccentrica.css', array(), '1.0.0', 'all');
 	wp_enqueue_script('customjs', get_template_directory_uri() . '/js/eccentrica.js', array(), '1.0.0', true);
-
 }
 
 add_action('wp_enqueue_scripts', 'eccentrica_script_enqueue');
+
+function eccentrica_theme_setup() {
+
+	add_theme_support('menus');
+
+	register_nav_menu('primary', 'Primary Header Navigation');
+	register_nav_menu('secondary', 'Footer Navigation');
+}
+
+add_action('init', 'eccentrica_theme_setup')
 
 ?>
